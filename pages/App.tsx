1b92f90bf7a49ecc9569639d39d74abe4cd5f8d9
FakhriadiRasyaad <161245848+FakhriadiRasyaad@users.noreply.github.com>
2024-10-11 23:54:41 +0700

    Update App.tsx

--- pages/App.tsx
+++ pages/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './login';
-import HomePage from './Home';  
+import HomePage from './home';  
 
 function App() {
   return (
